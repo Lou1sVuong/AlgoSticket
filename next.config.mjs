900cc2eae107c7f28acbee91029dc6736e0100dc
lou1s vuong <lou1svuong.dev@gmail.com>
2024-10-21 19:40:06 +0700

    initial project

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    domains: ["cdn.coin68.com", "encrypted-tbn0.gstatic.com"],
+  },
+};
 
 export default nextConfig;
